cc1cee4d0d90dca0b77751a9834a225c756b0f4f
Jayesh Nirve <nitinnirve@gmail.com>
2021-02-14 18:44:58 +0530

    remove flask, I cam just fetch file from this repo

--- tldrdict/update.py
+++ tldrdict/update.py
@@ -2,17 +2,6 @@ import json
 from os import path
 
 import requests
-from flask import Flask
-
-app = Flask(__name__, static_url_path="/static")
-
-
-@app.route('/')
-def commands_file():
-    if not path.exists("static/commands.txt"):
-        update_commands_file()
-    return app.send_static_file('commands.txt')
-
 
 def get_dict():
     r = requests.get(url="https://tldr.sh/assets/")
@@ -23,19 +12,19 @@ def get_dict():
     return commands
 
 
-@app.cli.command()
 def update_commands_file():
     """Run scheduled job to update commands file."""
     data = get_dict()
     try:
-        file = open('static/commands.txt', 'wt')
+        file = open('tldrdict/static/commands.txt', 'wt')
         file.write(str(data))
         file.close()
         print("Commands added")
 
     except:
+        print
         print("Unable to write to file")
 
 
 if __name__ == '__main__':
-    app.run()
+    update_commands_file()
