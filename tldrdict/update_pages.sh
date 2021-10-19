856960566f3df05b31f6eab2dd260a4b8323a55d
Jayesh Nirve <nirvejayesh@gmail.com>
2021-10-19 20:22:09 +0530

    feat: show timestamp since last update

--- tldrdict/update_pages.sh
+++ tldrdict/update_pages.sh
@@ -2,5 +2,5 @@
 
 git clone https://github.com/tldr-pages/tldr
 zip -r pages.zip tldr/pages
-echo "{\"version\": \"$(curl --upload-file pages.zip http://transfer.sh/pages.zip | cut -d/ -f 4)\"}"  > static/version.txt
+echo "{\"version\": \"$(curl --upload-file pages.zip http://transfer.sh/pages.zip | cut -d/ -f 4)\", \"lastUpdatedAt\": \"$(date +%s)\"}"  > static/version.txt
 rm -rf tldr pages.zip
