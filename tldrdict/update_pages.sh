fd2a0ecef14b04c412bdfccc00b3b8a909e57883
td <nirvejayesh@gmail.com>
2026-03-09 18:49:58 +0100

    chore: new link apparently

--- tldrdict/update_pages.sh
+++ tldrdict/update_pages.sh
@@ -2,7 +2,7 @@
 cd static
 
 # json format for all commands with name, languages and platform
-curl -s 'https://tldr.sh/assets/'|  jq --compact-output '[.commands[] | {command: .name, info: .targets | group_by(.os)[] | {os: .[] | .os, languages: [.[] | .language]}}]  | unique' > commands2.json
+curl -s 'https://github.com/tldr-pages/tldr/releases/latest/download/index.json'|  jq --compact-output '[.commands[] | {command: .name, info: .targets | group_by(.os)[] | {os: .[] | .os, languages: [.[] | .language]}}]  | unique' > commands2.json
 git clone https://github.com/tldr-pages/tldr
 zip -r allpages.zip tldr/pages*
 zip -r pages.zip tldr/pages # incase someone is still on the old version he should still get pages.zip
@@ -11,4 +11,4 @@ version=$(curl -s "https://api.github.com/repos/techno-disaster/tldr-flutter/com
 echo "{\"version\": \"$version\", \"lastUpdatedAt\": \"$(date +"%Y-%m-%d %T")\"}" > version.txt # deperacated, use versiom.json
 echo "{\"version\": \"$version\", \"lastUpdatedAt\": \"$(date +"%Y-%m-%d %T")\"}" > version.json
 # add a list of all supported languages to commands2.json
-curl -s 'https://tldr.sh/assets/' |  jq --compact-output '{supportedLanguages: [.commands[] | .language | .[]] | unique}' > temp.json && jq --compact-output --slurp add version.json temp.json | sponge version.json && rm temp.json
+curl -s 'https://github.com/tldr-pages/tldr/releases/latest/download/index.json' |  jq --compact-output '{supportedLanguages: [.commands[] | .language | .[]] | unique}' > temp.json && jq --compact-output --slurp add version.json temp.json | sponge version.json && rm temp.json
